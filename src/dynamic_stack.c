51174697d9f2ebb7dc96fea920deab02fa6e08a1
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2024-05-20 23:22:01 -0400

    Implement dynamic stack wrapping over array deque

---
+++ src/dynamic_stack.c
@@ -0,0 +1,25 @@
+#include "dynamic_stack.h"
+#include "dynamic_deque.h"
+#include <stdlib.h>
+
+DynamicStack dynamic_stack_init(size_t buf_size, const size_t data_size) {
+    DynamicDeque ddq = dynamic_deque_init(buf_size, data_size);
+    DynamicStack q = *(DynamicStack *)&ddq;
+    return q;
+}
+
+int dynamic_stack_empty(DynamicStack *q) { return dynamic_deque_empty((DynamicDeque *)q); }
+
+int dynamic_stack_push(DynamicStack *q, void *new_data) { return dynamic_deque_push_back((DynamicDeque *)q, new_data); }
+
+int dynamic_stack_pop(DynamicStack *q) { return dynamic_deque_pop_back((DynamicDeque *)q); }
+
+size_t dynamic_stack_capacity(DynamicStack *q) { return dynamic_deque_capacity((DynamicDeque *)q); }
+
+size_t dynamic_stack_length(DynamicStack *q) { return dynamic_deque_length((DynamicDeque *)q); }
+
+size_t dynamic_stack_data_size(DynamicStack *q) { return dynamic_deque_data_size((DynamicDeque *)q); }
+
+void *dynamic_stack_peek(DynamicStack *q) { return dynamic_deque_peek_back((DynamicDeque *)q); }
+
+void dynamic_stack_destroy(DynamicStack *q) { dynamic_deque_destroy((DynamicDeque *)q); }
---
+++ src/dynamic_stack.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#ifndef DYNAMIC_QUEUE_H
+#define DYNAMIC_QUEUE_H
+
+typedef struct {
+    void *internals;
+} DynamicStack;
+
+extern DynamicStack dynamic_stack_init(size_t buf_size, const size_t data_size);
+extern int dynamic_stack_empty(DynamicStack *q);
+extern int dynamic_stack_push(DynamicStack *q, void *new_data);
+extern int dynamic_stack_pop(DynamicStack *q);
+extern size_t dynamic_stack_capacity(DynamicStack *q);
+extern size_t dynamic_stack_length(DynamicStack *q);
+extern size_t dynamic_stack_data_size(DynamicStack *q);
+extern void *dynamic_stack_peek(DynamicStack *q);
+extern void dynamic_stack_destroy(DynamicStack *q);
+
+#endif // DYNAMIC_QUEUE_H
